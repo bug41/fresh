4c034806b0e5c1823ffc7a273440ffdf2d7b0202
bug41 <bugkinguswit@gmail.com>
2023-04-22 12:56:00 +0900

    array map 사용

--- app/list/page.js
+++ app/list/page.js
@@ -4,15 +4,20 @@ import Link from 'next/link'
 //import styles from './page.module.css'
 
 export default function List() {
+
+  let products = ['Tomatoes', 'Pasta', 'Coconut'];
   return (
     <div>
         <h4 className="title">상품목록</h4>
-        <div className="food">
-            <h4>상품1 $40</h4>
-        </div>
-        <div className="food">
-            <h4>상품2 $40</h4>
-        </div>
+        {
+            products.map((a,i)=>{
+                return (
+                    <div className="food">
+                        <h4>{products[i]} $1000</h4>
+                    </div>
+                )
+            })
+        }        
     </div>
   )
 }
