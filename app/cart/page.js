8df3a2ab7def84c32a3e20cef6a521cfbf8fe743
bug41 <bugkinguswit@gmail.com>
2023-04-23 15:10:33 +0900

    array , state 변경

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,20 +1,34 @@
 import {age,name} from './data'
 
 export default function Cart() {
+    let cart = ['Tomatoes', 'Pasta']
+
     return (
         <div>
             <h4 className="title">Cart</h4>            
-            <CartItem/>
-            <CartItem/>
+            <CartItem data={cart[0]} />
+            <CartItem data={cart[1]} />
+            <Banner data="삼성"/>
+            <Banner data="현다이"/>
+            <Banner data="롯다이"/>
+
+            <Button color='blue'/>
         </div>
     )
 }
 
+function Banner(props){
+    return <h5>{props.data}카드 결제 행사중</h5>
+}
 
-function CartItem(){
+function Button(props){
+    return <div style={{ width:100,height:100,backgroundColor:`${props.color}` }}></div>
+}
+
+function CartItem(props){
     return(
         <div className="cart-item">
-            <p>상품명 </p>
+            <p>{props.data} </p>
             <p>$40</p>
             <p>1개 {age}</p>
         </div>
--- app/list/page.js
+++ app/list/page.js
@@ -1,15 +1,19 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Link from 'next/link'
-//import styles from './page.module.css'
-import food0 from '/public/food0.png'
+'use client'
+
+import { useState } from "react";
 
 export default function List() {
 
   let products = ['Tomatoes', 'Pasta', 'Coconut'];
+  let [count, afterCount] = useState([0,0,0])
+ 
+
   return (
     <div>
         <h4 className="title">상품목록</h4>
+
+        
+
         {
             products.map((a,i)=>{
                 return (
@@ -17,6 +21,21 @@ export default function List() {
                         <img src={`/food${i}.png`} className="food-img"/>
                         {/* <Image src={food0} className="food-img"/> */}
                         <h4>{a} $1000</h4>
+                        <button 
+                            onClick={()=>{
+                                let copy = [...count]
+                                copy[i]++
+                                afterCount(copy)
+                            }}
+                        >+</button>
+                        <span> {count[i]} </span>
+                        <button onClick={()=>{ 
+                            if(count[i] > 1) {
+                                let copy = [...count]
+                                copy[i]--
+                                afterCount(copy)
+                            }
+                        }}>-</button>
                     </div>
                 )
             })
